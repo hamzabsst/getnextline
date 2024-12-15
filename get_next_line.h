7d0c41c2383c458f5ab7638032bbf10a3d4714e1
hamzabsst <hb.hamza.bst@gmail.com>
2024-12-15 11:29:55 +0100

    first

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,22 @@
 /*   By: hbousset < hbousset@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 21:51:23 by hbousset          #+#    #+#             */
-/*   Updated: 2024/12/14 21:51:24 by hbousset         ###   ########.fr       */
+/*   Updated: 2024/12/15 11:29:43 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+# endif
