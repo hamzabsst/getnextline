96b9c7561be483e04992309497cbdaf9e04e9b5c
Hamza Boussetta <hbousset@c2r3p6.1337.ma>
2024-12-16 13:55:13 +0100

    testing

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hbousset < hbousset@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 21:51:20 by hbousset          #+#    #+#             */
-/*   Updated: 2024/12/15 14:15:36 by hbousset         ###   ########.fr       */
+/*   Updated: 2024/12/16 13:54:41 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,5 @@
 
 char	*get_next_line(int fd)
 {
-	char	buffer;
-	char	line[9000000];
-	int		b;
-	int		i;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	i = 0;
-	b = read(fd, &buffer, 1);
-	while (b > 0)
-	{
-		line[i++] = buffer;
-		if (buffer == '\n')
-			break ;
-		b = read(fd, &buffer, 1);
-	}
-	line[i] = '\0';
-	if (b <= 0 && i == 0)
-		return (NULL);
-	return (ft_strdup(line));
+	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hbousset < hbousset@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 21:51:23 by hbousset          #+#    #+#             */
-/*   Updated: 2024/12/15 14:03:44 by hbousset         ###   ########.fr       */
+/*   Updated: 2024/12/16 13:54:46 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,5 @@
 # include <stdio.h>
 
 char	*get_next_line(int fd);
-char	*ft_strdup(const char *s);
-int	ft_strlen(const char *s);
 
 # endif
